8c8d9ea6adeab9cb8fc58a3e41b319d1d691e2bc
adonis7ejeda <adonis7ejeda@gmail.com>
2020-09-09 16:39:02 -0500

    Task 5 Airbnb_V.3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """List of states"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def api_status():
     """api_status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def some_stats():
+    """Some stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Place"),
+                    "states": storage.count("State"),
+                    "users": storage.count("State")})
