8a740602dce2caf4db07905ac8f623ef7c210199
ronniebm <ronnie.coding@gmail.com>
2020-09-10 20:44:17 -0500

    trying task13

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """Status API"""
 
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from models import storage
 from flask import jsonify
 from api.v1.views import app_views
@@ -10,6 +11,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}})
 
 
 @app.teardown_appcontext
